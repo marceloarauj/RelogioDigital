121065b323bb8bd67f3b8065bb5980f94f765940
marcelo-araujo <marcelo.araujo@avonale.com.br>
2020-04-29 19:24:08 -0300

    Start

---
+++ Arduino.cpp
@@ -0,0 +1,19 @@
+void setup(){
+
+}
+void loop(){
+
+}
+
+void AcenderLedUm(int numero){
+
+}
+void AcenderLedDois(int numero){
+
+}
+void AcenderLedTres(int numero){
+
+}
+void AcenderLedQuatro(int numero){
+
+}
